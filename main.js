3982e789ad2a3ac3fa68def747efca97730c07f3
Ivan <krya.krya.krya_yes_i_am@mail.ru>
2025-11-25 00:20:05 +0300

    Форматирование Имен

--- main.js
+++ main.js
@@ -1,32 +1,12 @@
 'use strict';
-let tasks = ["Выучить основы JS", "Помыть посуду", "Купить продукты"];
+const names = ["иван", "АННА", "петр", "ОЛЬГА"];
 
-function addTask(list, taskName) {
-  if (list.includes(taskName)) {
-    console.log("Задача уже существует");
-  } else {
-    list.push(taskName);
-    console.log(`Задача "${taskName}" добавлена`);
-  }
+function formatNames(namesArray) {
+  return namesArray.map(name => {
+    const lowerCaseName = name.toLowerCase();
+    const firstLetter = lowerCaseName.charAt(0).toUpperCase();
+    const restOfName = lowerCaseName.slice(1);
+    return firstLetter + restOfName;
+  });
 }
-
-function completeTask(list, taskName) {
-  const taskIndex = list.indexOf(taskName);
-  
-  if (taskIndex !== -1) {
-    list.splice(taskIndex, 1);
-    console.log(`Задача "${taskName}" выполнена и удалена из списка`);
-  } else {
-    console.log("Задача не найдена");
-  }
-}
-
-
-console.log("Начальный список задач:", tasks);
-addTask(tasks, "Почитать книгу");
-addTask(tasks, "Сделать зарядку");
-addTask(tasks, "Помыть посуду");
-console.log("Список после добавления:", tasks);
-completeTask(tasks, "Помыть посуду");
-completeTask(tasks, "Несуществующая задача");
-console.log("Финальный список:", tasks);
+console.log(formatNames(names));

3af15c2afc695695de2636175cfda8862f9a8c25
Ivan <krya.krya.krya_yes_i_am@mail.ru>
2025-11-24 22:45:24 +0300

    Профиль пользователя

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const userProfile ={
+    userName: 'developer123',
+    email: 'dev@example.com',
+    age: 25,
+    isLoggedIn: true
+};
+console.log(userProfile.email);
+
+userProfile.age = 26;
+
+console.log(userProfile.age);
+
+userProfile['user Location'] = 'New York';
+console.log(userProfile['user Location']);

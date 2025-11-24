27ab358234eba36f011b9e40cf3d1edac816e144
Ivan <krya.krya.krya_yes_i_am@mail.ru>
2025-11-24 23:34:36 +0300

    avarage value

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 'use strict';
-const salaries = {
-    ivan: 1200,
-    maria: 1500,
-    petr: 1100,
-    anna: 1600
-};
+const students = [
+  { name: "Иван", grades: [5, 4, 3, 5] },
+  { name: "Мария", grades: [5, 5, 5, 4] },
+  { name: "Алексей", grades: [3, 3, 4, 3] }
+];
 
-let totalSum = 0;
-for (const key in salaries) {
-    totalSum+=salaries[key];   
+function calculateAverage(grades) {
+  const sum = grades.reduce((total, grade) => total + grade, 0);
+  return sum / grades.length;
 }
-console.log(totalSum);
+function getStudentReport(studentsList) {
+  return studentsList.map(student => ({
+    name: student.name,
+    averageGrade: calculateAverage(student.grades)
+  }));
+}
+console.log(getStudentReport(students));
